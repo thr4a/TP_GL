bc736fe040cdc71b11a5d0510c36fd29276681fa
taakli <taakli@turing.local.isima.fr>
2022-10-06 14:26:27 +0200

    transfert de updateQuality dans Item.java

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,44 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                continue;
-            }
-            item.sellIn--;
-            switch(item.name){
-                case "Aged Brie":
-                    item.quality++;
-                    if (item.sellIn < 0) {
-                        item.quality++;
-                    }
-                    break;
-                
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    item.quality++;
-                    if (item.sellIn < 10) {
-                        item.quality++;
-                    }
-                    if (item.sellIn < 5) {
-                        item.quality++;
-                    }
-                    if (item.sellIn < 0) {
-                        item.quality = 0;
-                    }
-                    break;
-
-                default:
-                    item.quality--;
-                    if (item.sellIn < 0) {
-                        item.quality--;
-                    }
-                    break;
-            }
-            if (item.quality > 50) {
-                item.quality = 50;
-            }
-            if (item.quality < 0) {
-                item.quality = 0;
-            }
+            item.updateQuality();
         }
     }
 }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -14,6 +14,47 @@ public class Item {
         this.quality = quality;
     }
 
+    public void updateQuality() {
+            if (this.name.equals("Sulfuras, Hand of Ragnaros")) {
+                return;
+            }
+            this.sellIn--;
+            switch(this.name){
+                case "Aged Brie":
+                    this.quality++;
+                    if (this.sellIn < 0) {
+                        this.quality++;
+                    }
+                    break;
+                
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    this.quality++;
+                    if (this.sellIn < 10) {
+                        this.quality++;
+                    }
+                    if (this.sellIn < 5) {
+                        this.quality++;
+                    }
+                    if (this.sellIn < 0) {
+                        this.quality = 0;
+                    }
+                    break;
+
+                default:
+                    this.quality--;
+                    if (this.sellIn < 0) {
+                        this.quality--;
+                    }
+                    break;
+            }
+            if (this.quality > 50) {
+                this.quality = 50;
+            }
+            if (this.quality < 0) {
+                this.quality = 0;
+            }
+        }
+
    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
